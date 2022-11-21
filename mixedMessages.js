7923bdbcaacfd6184a494cd671858417b2bcf225
Carlos Lopez <carlosssolrac1@gmail.com>
2022-11-21 00:30:39 -0600

    add arrays with content in it

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,2 +1,6 @@
 //create 3 arrays that will hold different options
 
+let pronounArray = [He, She];
+let verbArray = [excited, sad, happy, crying, yelling, laughing];
+let nounArray = [basketball, house, game, score, team];
+
