023f3033d7869b549e60b4ef76208bd3ccb1b87b
Carlos Lopez <carlosssolrac1@gmail.com>
2022-11-21 01:33:09 -0600

    added seperate sentences with new elements in [ ]

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,9 +1,9 @@
 //create 3 arrays that will hold different options
 
 let subject = ["football", "basketball", "soccer", "tennis", "hockey", "golf", "tournament"];
-let day = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+let day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 let hour = ["10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00"];
-let amPM = ["am", "pm"];
+let amPM = ["AM", "PM"];
 
 //create randomizer that selects a random index from array
 
@@ -14,14 +14,17 @@ let randomAmPm = Math.floor(Math.random() * amPM.length);
 
 //console log the random index into text
 
-let randomMessage = () => {
+const randomMessage = () => {
 
 let v1 = subject[randomSubject];
 let v2 = day[randomDay];
 let v3 = hour[randomHour];
 let v4 = amPM[randomAmPm];
 
-return `You have a ${v1} game on ${v2} at ${v3} ${v4}.`;
+console.log(`You have a ${v1} game soon.`);
+console.log(`It's happening on ${v2}.`);
+console.log(`Be ready at ${v3} ${v4}.`);
 };
 
-console.log(randomMessage());
+
+randomMessage();
