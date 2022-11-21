e2e96b901c0f3b4b0c3e97f632ff05e4967547b0
Carlos Lopez <carlosssolrac1@gmail.com>
2022-11-21 01:03:08 -0600

    created index randomizers for each array

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,6 +1,29 @@
 //create 3 arrays that will hold different options
 
-let pronounArray = [He, She];
-let verbArray = [excited, sad, happy, crying, yelling, laughing];
-let nounArray = [basketball, house, game, score, team];
+let pronounArray = ["He", "She"];
+let verbArray = ["excited", "sad", "happy", "crying", "yelling", "laughing"];
+let nounArray = ["basketball", "game", "score", "team", "life"];
+
+
+//create randomizer that selects a random index from array
+
+let randomPronoun = (array) => {
+    let num = Math.floor(Math.random() * array.length) + 1;
+    return num;
+};
+
+let randomVerb = (array) => {
+    let num = Math.floor(Math.random() * array.length) + 1;
+    return num;
+};
+
+let randomNoun = (array) => {
+    let num = Math.floor(Math.random() * array.length) + 1;
+    return num;
+};
+
+
+console.log(randomPronoun(pronounArray));
+console.log(randomVerb(verbArray));
+console.log(randomVerb(nounArray));
 
